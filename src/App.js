5bb21035656d11f405da87e7af675e8f7798e797
Maryussuperboy <martsyalis@gmail.com>
2017-11-08 12:12:40 -0800

    mvp

--- src/App.js
+++ src/App.js
@@ -3,21 +3,60 @@ import logo from './logo.svg';
 import './App.css';
 import styled from 'styled-components'; 
 
-const omdbKey = process.env.REACT_APP_OMDB_API_KEY || '';
+const omdbKey = process.env.REACT_APP_OMDB_API_KEY || '3db77742';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
+      isNotFound: false,
       search: 'Search For Your Favorite Movie',
-      isLoading: true,
+      loading: false,
+      isLoading: false,
       isFocused: false,
-      
+      results: []
     };
   }
 
+  onEnter = (e) => {
+    if (e.charCode == 13) {
+      this.doSearch(this.state.search);
+      this.setState({ search: '' });
+    }
+  }
+
+  onSearchChange = (e) => {
+    this.setState({ search: e.target.value });
+  }
+
   onFocus = () => {
+    this.setState({ search : '' });
     this.setState({ isFocused: true });
+  }
+
+  offFocus = () => {
+    this.setState({ isFocused: false });
+  }
+
+  async doSearch(search) {
+    console.log('searching');
+    this.setState({ results: [] });
+    this.setState({ isLoading: true });
+    const url = encodeURI(`http://www.omdbapi.com/?s=${this.state.search}&plot=short&r=json&apikey=${omdbKey}`);
+    console.log(url);
+    const response = await fetch(url);
+    const body = await response.json();
+    setTimeout(()=>{
+      if(body.Response === 'True'){
+        this.setState({ isNotFound: false });
+        this.setState({ isLoading: false });
+        this.setState({ results: body.Search });
+      } else{
+        this.setState({ isLoading: false });
+        this.setState({ isNotFound: true });
+        this.setState({ results: [] });
+      }
+    },1000);
   }
 
   render() {
@@ -29,16 +68,31 @@ class App extends Component {
         </header>
          
         <div className="main" style={{ display:'flex', justifyContent:'center' }}>
-          <div className="wraper" style={{ display:'flex', flexDirection:'column' }}>
+          <div className="wrraper">
             <SearchInput 
-              isFocused={this.state.isFocused}
               name='search'
-              value={this.state.search}
+              isFocused={this.state.isFocused}
+              onKeyPress={this.onEnter}
               onFocus={this.onFocus}
+              onBlur={this.offFocus}
+              value={this.state.search}
+              onChange={this.onSearchChange}
             />
-            <ul>
-              <li>Movies go here</li>
-            </ul>
+            <Movies results={this.state.results}/>
+
+            <Loading isLoading={this.state.isLoading}>
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">LoadingResults</h1>
+            </Loading>
+            <NotFound isNotFound={this.state.isNotFound}>
+              <div style={{ display:'flex', justifyContent:'center' }}>
+                <Pug alt={''}
+                  src="https://static1.squarespace.com/static/594974c0e58c62484cbd42f9/t/594986a45a730f2283302fc9/1497992070607/img_2835.jpg"
+                />
+              </div>
+              <h1 className="App-title">No results were found to match the search</h1>
+            </NotFound>
+
           </div>
         </div>
         
@@ -49,10 +103,38 @@ class App extends Component {
   }
 }
 
+
+class Movies extends Component {
+  render() {
+    const { results } =this.props;
+    return(
+      <ol>
+        {results.map((result, i) => <li key={i}>{result.Title}</li>)}
+      </ol>
+    );
+  }
+}
+
+
 export default App;
 
+const Pug = styled.img`
+width:80%;
+height:400px;
+`;
+
+const NotFound = styled.div`
+display: ${props => props.isNotFound ? 'flex' : 'none'};
+flex-direction: column;
+`;
+
+const Loading = styled.div`
+  display: ${props => props.isLoading ? 'flex' : 'none'};
+  flex-direction: column;
+`;
+
 const SearchInput = styled.input`
-  width: ${props => props.isFocused ? '500px' : '300px'} ;
-  height: ${props => props.isFocused ? '80px' : '30px'};
+  width: ${props => props.isFocused ? '350px' : '300px'} ;
+  height: ${props => props.isFocused ? '40px' : '30px'};
   text-align: center;
 `;
