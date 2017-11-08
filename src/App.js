a9695bfb72b3a890f45293094df2993f71d0129a
Andrew Bodey <a.s.bodey@gmail.com>
2017-11-08 13:22:11 -0800

    console log, need better var names!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
+// console log the values to see what I am passing and getting
+//give an if else for getting a null response to array.map to
 import React, { Component } from 'react';
 
 const omdbKey = process.env.REACT_APP_OMDB_API_KEY;
-// let resource = null;
-// let items = null;
 
 export default class App extends Component {
   constructor(){
     super();
     this.state = {
       items: [],
-      resource: 'Title',
+      resource: 'movie',
       loading: false
     };
   }
@@ -17,32 +17,49 @@ export default class App extends Component {
 
 
   //   //like an onload function in react
-  componetDidMount(){
+  componentDidMount(){
     this.loadResource(this.state.resource);
   }
-
-  async loadResource (resource) {
+  
+  async loadResource (filter) {
     this.setState({ loading: true });
-    const response = await fetch(`http://www.omdbapi.com/?s=Star%20Wars&plot=short&r=json&apikey=${omdbKey}`);
+    console.log('=====filter', filter);
+    const response = await fetch(`http://www.omdbapi.com/?s=${filter}&plot=short&r=json&apikey=${omdbKey}`);
     const body = await response.json();
     this.setState({
-      items: body.results,
+      items: body.Search,
       loading: false
     });
   }
-
+  
+  changeResource(change) {
+    console.log('=====changeres', change);
+    this.setState({ resource: change }, () => {
+      return this.loadResource(change);
+    });
+  }
+  
   render() {
     const { resource, loading, items } = this.state;
-    const choices = ['Title'];
+    const choices = ['Title', 'Year', 'Goonies'];
 
     const list = (
       <ul>
-        {items.map(item => <li key={item.name}>{item.name}</li>)}
+        {items.map(item => <li key={item.imdbID}>{item.Title}</li>)}
       </ul>
     );
-
+    
+    const load = <div> Loading... </div>;
+    
     return (
-      <div className="App">
+      <section>
+        <Header filter={resource} onResourceChange={filter => {
+          console.log('=====filterasfg', filter);
+          this.setState({ filter });
+          this.setState({ resource: filter }, () => {
+            return this.loadResource(filter);
+          });
+        }}/>
         {choices.map(choice => {
           return <button key={choice} disabled={choice === resource}
             onClick={() => this.changeResource(choice)}
@@ -50,7 +67,23 @@ export default class App extends Component {
             {choice}
           </button>;
         })}
-      </div>
+        <div>{items.length} {resource}</div>
+        {loading ? load : list}
+      </section>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    const { resource, onResourceChange } = this.props;
+    return (
+      <header className='App-header'>
+        <input value={resource} onChange={({ target }) =>{
+          console.log('=====target.value', target.value);
+          onResourceChange(target.value);
+        }}/>
+      </header> 
     );
   }
 }
