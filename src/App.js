4ad4fe3f3a3c0520d22bd45722f498b80b493413
Shane Moyo <smoyo@pdx.edu>
2017-11-08 10:20:29 -0800

    made some basic methods

--- src/App.js
+++ src/App.js
@@ -2,7 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const omdbKey = process.env.REACT_APP_OMDB_API_KEY;
+
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      items: [],
+      resource: 'people',
+      loading: false
+    };
+  }
+
+  componentDidMount(){
+    this.loadResource(this.state.resource);
+  }
+
+  async loadResource(resource){
+    this.setState({ loading: true });
+    const response = await fetch();
+    const body = await response.json();
+    this.setState({
+      items: body.results,
+      loading: false
+    });
+  }
+   
+  changeResource(resource){
+    this.setState({ resource }, () => {
+      this.loadResource(resource);
+    });
+  }
+
   render() {
     return (
       <div className="App">
