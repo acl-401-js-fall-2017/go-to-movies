87c0f46b18e984ca6aff5e572e0ccc6fddeb50b7
Andrew Bodey <a.s.bodey@gmail.com>
2017-11-08 11:15:06 -0800

    non crashing app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+const omdbKey = process.env.REACT_APP_OMDB_API_KEY;
+// let resource = null;
+// let items = null;
+
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      items: [],
+      resource: 'Title',
+      loading: false
+    };
+  }
+
+
+
+  //   //like an onload function in react
+  componetDidMount(){
+    this.loadResource(this.state.resource);
+  }
+
+  async loadResource (resource) {
+    this.setState({ loading: true });
+    const response = await fetch(`http://www.omdbapi.com/?s=Star%20Wars&plot=short&r=json&apikey=${omdbKey}`);
+    const body = await response.json();
+    this.setState({
+      items: body.results,
+      loading: false
+    });
+  }
+
   render() {
+    const { resource, loading, items } = this.state;
+    const choices = ['Title'];
+
+    const list = (
+      <ul>
+        {items.map(item => <li key={item.name}>{item.name}</li>)}
+      </ul>
+    );
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {choices.map(choice => {
+          return <button key={choice} disabled={choice === resource}
+            onClick={() => this.changeResource(choice)}
+          >
+            {choice}
+          </button>;
+        })}
       </div>
     );
   }
 }
 
-export default App;
