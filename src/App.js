1511fd95a0cb1081d40436695c31a4474f1d85c2
Robin Miller <rmillerpdx@gmail.com>
2017-11-08 09:54:57 -0800

    successfully populate static query

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+require('dotenv').config();
+
+const omdbKey = process.env.REACT_APP_OMDB_API_KEY;
 
 class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      items : [],
+      query : 'Tornado'
+    };
+  }
+
+  componentDidMount() {
+    this.loadResource();
+  }
+
+  async loadResource() {
+    const response = await fetch(`http://www.omdbapi.com/?s=${this.state.query}&apikey=${omdbKey}`)
+    const body = await response.json();
+    console.log('BODY IS', body);
+    this.setState({ items: body.Search })
+  }
+  
+
   render() {
+    const { items } = this.state;
+
+    const list = (
+      <ul>
+        {items.map(item => <li key={item.imdbID}>{item.Title}</li>)}  
+      </ul>
+    );
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">MovieSearch.org</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          :( UNDER CONSTRUCTION :(
         </p>
+          {list}
       </div>
     );
   }
