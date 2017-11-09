0203e6376eef248f95cf7c195357c9e263ac23c9
Zac-L <Zachlichens@gmail.com>
2017-11-09 10:54:11 -0800

    added comment about imdb link

--- src/omdbApp.js
+++ src/omdbApp.js
@@ -43,6 +43,9 @@ export default class OmdbApp extends Component {
         {items.map(item => <div key={item.imdbID}>
           <h2> {item.Title} </h2>
           <h3> Released: {item.Year} </h3>
+
+          {/* This will reference imdb website, adds the imdbID that omdb sends us back, 
+              and uses the Title that omdb sends to display that as the link name */}
           <h4> IMDB Link: <a href={'http://www.imdb.com/title/' + item.imdbID}>{item.Title}</a> </h4>
           <img src={item.Poster} alt='Movie Poster' />
         </div>)}
