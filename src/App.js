71bda798e61cc13750357971fda07c26da26df52
mirrorapps <eli.speigel@gmail.com>
2017-11-12 16:37:46 -0800

    searchbar appears in production browser as expected

--- src/App.js
+++ src/App.js
@@ -6,10 +6,24 @@ const omdbKey = process.env.REACT_APP_OMDB_API_KEY;
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <SearchBar />
-        <MovieList />
+        {/* <MovieList /> */}
       </div>
+    );
+  }
+}
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form className="SearchBar">
+        <label>
+          Title:
+          <textarea />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
