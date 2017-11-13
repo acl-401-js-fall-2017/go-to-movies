ced4614280b3953c5570f1fe59c0c574f206c491
mirrorapps <eli.speigel@gmail.com>
2017-11-12 16:30:24 -0800

    app component

--- src/App.js
+++ src/App.js
@@ -4,5 +4,12 @@ import './App.css';
 const omdbKey = process.env.REACT_APP_OMDB_API_KEY;
 
 export default class App extends Component {
-  
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <MovieList />
+      </div>
+    );
+  }
 }
